e98e76a84c7b25ef05977ae4c6efe3b689148aeb
Adam <adam@crocodic.com>
2018-01-20 14:59:27 +0700

    init

---
+++ resources/views/export_import.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Import-Export Data</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <br />
+      <div class="row">
+        <div class="col-md-8"></div>
+        <div class="col-md-2">
+          <button class="btn btn-primary">Import</button>
+        </div>
+        <div class="col-md-2">
+          <button class="btn btn-success">Export</button>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
